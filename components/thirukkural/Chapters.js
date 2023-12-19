09d6cdc5e360b2036306ec6d135ff27ce9933e6a
Thanga Ganapathy <ganapathy888@gmail.com>
2023-12-20 01:13:08 +0530

    Add new thirukkural chapter

--- components/thirukkural/Chapters.js
+++ components/thirukkural/Chapters.js
@@ -4,6 +4,20 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { thirukkural } from '@/data/thirukkural';
 
 export default function Chapters() {
+  const chapters = thirukkural.chapters.sort((a, b) => {
+    const nameA = a.name;
+    const nameB = b.name;
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+
   return (
     <>
       <Typography variant="h6" sx={{ display: 'flex', alignItems: 'center' }}>
@@ -14,7 +28,7 @@ export default function Chapters() {
       <Box mt={2}>
         <Box component="table" sx={{ borderCollapse: 'collapse' }}>
           <Box component="tbody">
-            {thirukkural.chapters.map((ch, i) => (
+            {chapters.map((ch, i) => (
               <Box component="tr" key={i}>
                 <Box component="td" sx={{ border: '1px solid black', p: 1 }}>
                   {i + 1}
