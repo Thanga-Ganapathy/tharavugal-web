e837c81e24df28009b1e955d14835a3dbd95602e
Thanga Ganapathy <ganapathy888@gmail.com>
2023-12-21 15:53:49 +0530

    Add new chapter into thirukkural

---
+++ data/thirukkural/index.js
@@ -0,0 +1,76 @@
+import kurals1 from './chapters/கடவுள்-வாழ்த்து';
+import kurals2 from './chapters/வான்-சிறப்பு';
+import kurals3 from './chapters/நீத்தார்-பெருமை';
+import kurals4 from './chapters/அறன்-வலியுறுத்தல்';
+import kurals5 from './chapters/இல்-வாழ்க்கை';
+import kurals6 from './chapters/வாழ்க்கைத்-துணைநலம்';
+import kurals7 from './chapters/மக்கட்பேறு';
+import kurals8 from './chapters/அன்புடைமை';
+import kurals9 from './chapters/விருந்தோம்பல்';
+import kurals10 from './chapters/இனியவை-கூறல்';
+
+export const thirukkural = {
+  chapters: [
+    {
+      name: 'கடவுள் வாழ்த்து',
+      slug: 'கடவுள்-வாழ்த்து',
+      translations: [{ id: 'en', text: 'The Praise of God' }],
+      kurals: kurals1,
+    },
+    {
+      name: 'வான் சிறப்பு',
+      slug: 'வான்-சிறப்பு',
+      translations: [{ id: 'en', text: 'The Excellence of Rain' }],
+      kurals: kurals2,
+    },
+    {
+      name: 'நீத்தார் பெருமை',
+      slug: 'நீத்தார்-பெருமை',
+      translations: [{ id: 'en', text: 'The Greatness of Ascetics' }],
+      kurals: kurals3,
+    },
+    {
+      name: 'அறன் வலியுறுத்தல்',
+      slug: 'அறன்-வலியுறுத்தல்',
+      translations: [{ id: 'en', text: 'Assertion of the Strength of Virtue' }],
+      kurals: kurals4,
+    },
+    {
+      name: 'இல் வாழ்க்கை',
+      slug: 'இல்-வாழ்க்கை',
+      translations: [{ id: 'en', text: 'Domestic Life' }],
+      kurals: kurals5,
+    },
+    {
+      name: 'வாழ்க்கைத் துணைநலம்',
+      slug: 'வாழ்க்கைத்-துணைநலம்',
+      translations: [
+        { id: 'en', text: 'The Goodness of the Help to Domestic life' },
+      ],
+      kurals: kurals6,
+    },
+    {
+      name: 'மக்கட்பேறு',
+      slug: 'மக்கட்பேறு',
+      translations: [{ id: 'en', text: 'The Obtaining of Sons' }],
+      kurals: kurals7,
+    },
+    {
+      name: 'அன்புடைமை',
+      slug: 'அன்புடைமை',
+      translations: [{ id: 'en', text: 'The Possession of Love' }],
+      kurals: kurals8,
+    },
+    {
+      name: 'விருந்தோம்பல்',
+      slug: 'விருந்தோம்பல்',
+      translations: [{ id: 'en', text: 'Cherishing Guests' }],
+      kurals: kurals9,
+    },{
+      name: 'இனியவை கூறல்',
+      slug: 'இனியவை-கூறல்',
+      translations: [{ id: 'en', text: 'The Utterance of Pleasant Words' }],
+      kurals: kurals10,
+    },
+  ],
+};
