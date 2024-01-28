4deb8367e23b0eed971d3c28974f1bdcc371467c
Thanga Ganapathy <ganapathy888@gmail.com>
2024-01-29 03:27:49 +0530

    Change section links styles

--- components/home/Sections.js
+++ components/home/Sections.js
@@ -2,25 +2,40 @@ import { Box } from '@mui/material';
 import Link from 'next/link';
 import HeadingWithDivider from '../HeadingWithDivider';
 
+function SectionBox({ title, href }) {
+  return (
+    <Box
+      component="a"
+      href={href}
+      sx={{
+        p: 2,
+        m: 1,
+        fontWeight: 'bold',
+        color: 'white',
+        backgroundColor: '#001f3f',
+        borderRadius: '8px',
+        textDecoration: 'none',
+        textTransform: 'capitalize',
+        '&:hover': {
+          backgroundColor: 'rgba(1, 255, 112, 0.5)',
+          color: 'black',
+        },
+      }}
+    >
+      {title}
+    </Box>
+  );
+}
+
 export default function Sections() {
   return (
     <Box p={1}>
-      <HeadingWithDivider title="Sections" />
-      <Box>
-        <ul>
-          <li>
-            <Link href="/open-discussions">Open Discussions</Link>
-          </li>
-          <li>
-            <Link href="/open-issues">Open Issues</Link>
-          </li>
-          <li>
-            <Link href="/entities">Entities</Link>
-          </li>
-          <li>
-            <Link href="/facts">Facts</Link>
-          </li>
-        </ul>
+      <HeadingWithDivider title="Sections" sx={{ mb: 2 }} />
+      <Box sx={{ mt: 2, display: 'flex', flexWrap: 'wrap' }}>
+        <SectionBox title="Open Discussions" href="/open-discussions" />
+        <SectionBox title="Open Issues" href="/open-issues" />
+        <SectionBox title="Entities" href="/entities" />
+        <SectionBox title="Facts" href="/facts" />
       </Box>
     </Box>
   );
