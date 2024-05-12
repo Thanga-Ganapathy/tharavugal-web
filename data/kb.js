552c159c0b77e0b80343710af0a2553585c49e26
Thanga Ganapathy <ganapathy888@gmail.com>
2024-05-12 11:55:38 +0530

    Update kb

--- data/kb.js
+++ data/kb.js
@@ -8,9 +8,37 @@ const kbData = [
     </ul>`,
   },
   {
+    title: 'Core Members',
+    content: `<ul>
+    <li>The core members govern the organization.</li>
+    <li>They make all the decisions.</li>
+    <li>They manage the funds.</li>
+    </ul>`,
+  },
+  {
+    title: 'General Members',
+    content: `<ul>
+    <li>The general members are those who are invited and designated by the core members, and they volunteerly contribute to any aspect of the organization.</li>
+    <li>They contribute to any projects, participate in research developments, or donate funds.</li>
+    <li>They will have limited access to the resources and no execution powers.</li>
+    </ul>`,
+  },
+  {
+    title: 'Are there any premium memberships available?',
+    content: `Never.`,
+  },
+  {
     title: 'How many members are there?',
-    content: `Please refer the <a href="/statistics">Statistics</a> page.`
-  }
+    content: `Please refer the <a href="/statistics">Statistics</a> page.`,
+  },
+  {
+    title: 'Who can be a core member?',
+    content: `<ul>
+    <li>Anyone with ethical (அறம்).</li>
+    <li>Must be an expert in any one field</li>
+    <li>Invited only through the recommendation of the existing core members.</li>
+    </ul>`,
+  },
 ];
 
 export default kbData;
