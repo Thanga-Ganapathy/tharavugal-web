86513c5cba8a5698ab0e4f9fb6f3990cb66e5445
Thanga Ganapathy <ganapathy888@gmail.com>
2025-01-16 02:17:42 +0530

    Fix event location parents list

--- components/Events/Event.js
+++ components/Events/Event.js
@@ -10,36 +10,38 @@ import {
 import { format } from 'date-fns';
 import ActionMenu from './ActionMenu';
 import { useRouter } from 'next/router';
-import { TZDate } from "@date-fns/tz";
+import { TZDate } from '@date-fns/tz';
 import Link from '../app/Link';
 import ImageIcon from '@mui/icons-material/Image';
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 import AnchorIcon from '@mui/icons-material/Anchor';
 import LinkIcon from '@mui/icons-material/Link';
 
-export default function Event({ data }) {
+const InfoTooltip = ({ title, Icon }) => (
+  <Tooltip sx={{ mr: 2 }} title={title}>
+    <Icon fontSize="small" color="disabled" />
+  </Tooltip>
+);
+
+const Event = ({ data }) => {
   const router = useRouter();
 
   const handleExplore = (name, val) => {
     router.push(`/events/search?${name}=${val}`);
   };
 
-  return (
-    <Card variant="outlined" sx={{ mb: 1 }}>
-      <CardHeader
-        action={<ActionMenu />}
-        title={<Link href={'/events/' + data.slug}>{data.title}</Link>}
-      />
-      <CardContent>
-        <Box>
-          {data.locations.map((l) => (
+  const renderLocations = (locations) =>
+    locations.map((loc) => {
+      const locs = [loc, ...loc.parentLocations];
+      return (
+        <Box key={loc.id} sx={{ display: 'block', mt: 1 }}>
+          {locs.map((l) => (
             <Chip
               color="info"
               variant="outlined"
               key={l.id}
               label={l.name}
               sx={{
-                mt: { xs: 1 },
                 mr: 1,
                 height: 'auto',
                 '& .MuiChip-label': {
@@ -52,20 +54,32 @@ export default function Event({ data }) {
             />
           ))}
         </Box>
+      );
+    });
+  
+  const renderCategories = (categories) =>
+    categories.map((c) => (
+      <Chip
+        variant="outlined"
+        color="default"
+        key={c.id}
+        label={c.name}
+        sx={{ mt: { xs: 1 }, mr: 1 }}
+        size="small"
+        onClick={() => handleExplore('tag', c.id)}
+      />
+    ));
+
+  return (
+    <Card variant="outlined" sx={{ mb: 1 }}>
+      <CardHeader
+        action={<ActionMenu />}
+        title={<Link href={`/events/${data.slug}`}>{data.title}</Link>}
+      />
+      <CardContent>
+        <Box>{renderLocations(data.locations)}</Box>
         {data.categories && (
-          <Box sx={{ mt: 2 }}>
-            {data.categories.map((c) => (
-              <Chip
-                variant="outlined"
-                color="default"
-                key={c.id}
-                label={c.name}
-                sx={{ mt: { xs: 1 }, mr: 1 }}
-                size="small"
-                onClick={() => handleExplore('tag', c.id)}
-              />
-            ))}
-          </Box>
+          <Box sx={{ mt: 2 }}>{renderCategories(data.categories)}</Box>
         )}
 
         <Box
@@ -77,25 +91,14 @@ export default function Event({ data }) {
           }}
         >
           <Box>
-            <Tooltip sx={{ mr: 2 }} title="Images (0)">
-              <ImageIcon fontSize="small" color="disabled" />
-            </Tooltip>
-            <Tooltip sx={{ mr: 2 }} title="Videos (0)">
-              <PlayCircleFilledIcon fontSize="small" color="disabled" />
-            </Tooltip>
-            <Tooltip sx={{ mr: 2 }} title="References (0)">
-              <AnchorIcon fontSize="small" color="disabled" />
-            </Tooltip>
-            <Tooltip sx={{ mr: 2 }} title="Links (0)">
-              <LinkIcon fontSize="small" color="disabled" />
-            </Tooltip>
+            <InfoTooltip title="Images (0)" Icon={ImageIcon} />
+            <InfoTooltip title="Videos (0)" Icon={PlayCircleFilledIcon} />
+            <InfoTooltip title="References (0)" Icon={AnchorIcon} />
+            <InfoTooltip title="Links (0)" Icon={LinkIcon} />
           </Box>
           <Box sx={{ textAlign: 'right' }}>
             <Typography variant="span" fontSize={12} color="text.secondary">
-              {format(
-                new TZDate(data.startedAt, data.startTz),
-                'yyyy-MM-dd hh:mm:ss aa'
-              )}{' '}
+              {format(new TZDate(data.startedAt, data.startTz), 'yyyy-MM-dd hh:mm:ss aa')}{' '}
               {data.startTz}
             </Typography>
           </Box>
@@ -103,4 +106,6 @@ export default function Event({ data }) {
       </CardContent>
     </Card>
   );
-}
+};
+
+export default Event;
--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,4 +1,50 @@
 import { getDB } from '@/lib/db';
+import { reverse } from '@opentf/std';
+
+async function getParentLocations(locationIds, locationsCollection) {
+  // Recursive function to fetch parent locations for each locationId
+  const fetchParentLocations = async (parentId) => {
+    if (!parentId) {
+      return []; // If no parent, return an empty array
+    }
+
+    // Find the parent location by parentId
+    const parentLocation = await locationsCollection.findOne(
+      { id: parentId },
+      { projection: { _id: 0 } }
+    );
+
+    if (!parentLocation) {
+      return []; // If no parent location found, return an empty array
+    }
+
+    // Recursively find the parent of the parent
+    const parents = await fetchParentLocations(parentLocation.parentId);
+
+    // Return the parent locations in hierarchical order (top to bottom)
+    return [...parents, parentLocation]; // Add the current parent at the end of the array
+  };
+
+  // For each locationId in the locations array, fetch parent locations
+  const result = [];
+  for (const locationId of locationIds) {
+    const location = await locationsCollection.findOne({ id: locationId });
+
+    if (location?.parentId) {
+      // Fetch the parent locations for this specific location
+      const parentLocations = await fetchParentLocations(location.parentId);
+
+      // Store the parent locations in result
+      result.push({
+        id: location.id,
+        name: location.name,
+        parentLocations: reverse(parentLocations),
+      });
+    }
+  }
+
+  return result;
+}
 
 export default async function handler(req, res) {
   const db = await getDB();
@@ -17,20 +63,31 @@ export default async function handler(req, res) {
             status: 'Published',
           },
         },
-        {
-          $lookup: {
-            from: 'locations', // The collection to join with
-            let: { locations: '$locations' }, // Define 'locations' variable
-            pipeline: [
-              {
-                $match: {
-                  $expr: { $in: ['$id', '$$locations'] }, // Use $$locations as a variable inside the lookup pipeline
-                },
-              },
-            ],
-            as: 'locations', // The name of the new array field containing matched location documents
-          },
-        },
+        // {
+        //   $lookup: {
+        //     from: 'locations', // The collection to join with
+        //     let: { locations: '$locations' }, // Define 'locations' variable
+        //     pipeline: [
+        //       {
+        //         $match: {
+        //           $expr: { $in: ['$id', '$$locations'] }, // Use $$locations as a variable inside the lookup pipeline
+        //         },
+        //       },
+        //       {
+        //         $graphLookup: {
+        //           from: 'locations',  // The same collection to perform recursive lookup
+        //           startWith: '$parentId',  // Field to start the lookup from (parentId)
+        //           connectFromField: 'parentId',  // Field in the current document to match against
+        //           connectToField: 'id',  // Field in the joined document to match against
+        //           as: 'parentLocations',  // Name of the new array field to store the hierarchy
+        //           // depthField: 'depth',  // Optional: Include a depth field to track the level of nesting
+        //           // maxDepth: 5  // Optional: Limit the depth of recursion to prevent infinite loops
+        //         }
+        //       }
+        //     ],
+        //     as: 'locations', // The name of the new array field containing matched location documents
+        //   },
+        // },
         {
           $lookup: {
             from: 'event-categories', // The collection to join with
@@ -56,16 +113,37 @@ export default async function handler(req, res) {
         },
         {
           $project: {
+            _id: 0,
+            id: 1,
             title: 1,
             slug: 1,
-            locations: { id: 1, name: 1 },
+            locations: 1,
             startedAt: 1,
             startTz: 1,
-            categories: 1,
+            categories: { id: 1, name: 1 },
           },
         },
       ]);
-      const data = JSON.parse(JSON.stringify(await cursor.toArray()));
+
+      const locationsCollection = db.collection('locations');
+      let data = JSON.parse(JSON.stringify(await cursor.toArray()));
+
+      for (const e of data) {
+        // Await the result of getParentLocations for each location in the `locations` array
+        e.locations = await getParentLocations(
+          e.locations,
+          locationsCollection
+        );
+      }
+
+      // data = data.map(async (e) => {
+      //   e.locations = await getParentLocations(
+      //     e.locations,
+      //     locationsCollection
+      //   );
+
+      //   return e;
+      // });
       output = res.status(200).json({
         data,
       });
